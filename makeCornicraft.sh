d2d86f85a48128bad5790ff1229bf6a3d001f33a
charles66820 <charles.goedefroit@gmail.com>
2021-12-31 00:32:38 +0100

    add cornicraft compile script

---
+++ makeCornicraft.sh
@@ -0,0 +1,6 @@
+make -f cornicraftBiome/Makefile clean
+make -f cornicraftCore/Makefile clean
+make -f cornicraftRessource/Makefile clean
+make -f cornicraftBiome/Makefile
+make -f cornicraftCore/Makefile
+make -f cornicraftRessource/Makefile
